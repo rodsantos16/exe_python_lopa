8d74c6a19ee06a94d52cd13b30b70b4ba8ac5154
mateus <mateusmoreira1901@gmail.com>
2025-02-12 22:56:55 -0300

    seila

---
+++ exercicio2.py
@@ -0,0 +1,7 @@
+#Faça um programa que converta a moeda REAL em DÓLAR
+
+dolar = 6.0973
+conversao = (float(input('Digite o valor em R$ que gostaria de converter para dolar')))
+print(conversao)
+total = dolar*conversao
+print(total)
